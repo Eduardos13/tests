f4841831d062211858ed282169c3038f82e7fea3
Eduardos13 <edik.kozelsky@gmail.com>
2024-04-01 21:20:54 -0400

    switch case examples

---
+++ switch/task-2.js
@@ -0,0 +1,38 @@
+const number = Number.parseInt(Math.random() * 10);
+
+switch (number) {
+    case 0:
+        console.log("Better not to");
+        break;
+    case 1:
+        console.log("Maybe");
+        break;
+    case 2:
+        console.log("Tottaly yes");
+        break;
+    case 3:
+        console.log("Never");
+        break;
+    case 4:
+        console.log("Ask me later");
+        break;
+    case 5:
+        console.log("I don't know");
+        break;
+    case 6: 
+        console.log("More yes then no");
+        break;
+    case 7:
+        console.log("More no then yes");
+        break;
+    case 8: 
+        console.log("You know better");
+        break;
+    case 9:
+        console.log("Don't do that");
+        break;
+    default:
+        console.log("Another question please");
+};
+
+
